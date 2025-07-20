487758ba675dae5a7f1bd6962514e1d7ad53e978
Nagendra678 <148514633+Nagendra678@users.noreply.github.com>
2025-07-20 19:03:16 +0530

    Update Item.java

--- src/main/java/com/makkajai/Item.java
+++ src/main/java/com/makkajai/Item.java
@@ -18,16 +18,21 @@ public class Item {
     private boolean isExempt(String name) {
         return name.contains("book") || name.contains("chocolate") || name.contains("pill");
     }
-
+    //** in this block of code every Local,Luxery,local+Imported,Luxery+Imported all the items are checked here 
     public double getTax() {
         double tax = 0.0;
-        if (!exempt) tax += price * 0.10;
-        if (imported) tax += price * 0.05;
+        if (!exempt) tax += price * 0.10; //luxery items here 10 percentage tax on that 
+        if (imported) tax += price * 0.05; // luxery items with import here is 10 percentage luxery tax and 5 percentage imported duty
         return roundTax(tax);
+
+        //* here not tax in daily items means local items which is considered in this code as exempt but 
+        //* the local item is imported there is a tax for that is only 5 percentage  
     }
 
     private double roundTax(double tax) {
         return Math.ceil(tax * 20.0) / 20.0;
+        
+       // ** Rounds the given tax amount up to the nearest 0.05 using ceiling logic
     }
 
     public double getTotalPrice() {

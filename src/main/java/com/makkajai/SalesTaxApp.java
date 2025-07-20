3bc744929409e989298fe69a95318c0a8d5b2e28
Nagendra678 <148514633+Nagendra678@users.noreply.github.com>
2025-07-20 18:43:12 +0530

    Update SalesTaxApp.java

--- src/main/java/com/makkajai/SalesTaxApp.java
+++ src/main/java/com/makkajai/SalesTaxApp.java
@@ -3,13 +3,16 @@ package com.makkajai;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+// here use regex to get a match input according to the  string input
+//** pettern is used to define the pattern of the string .
+//** matcher use the pattern to match the string input or parts of string value is match according to pattern
+//**or not
 public class SalesTaxApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Receipt receipt = new Receipt();
 
-        System.out.println("Enter items (e.g. '1 book at 12.49'), blank line to finish:");
+        System.out.println("Enter items:");
         while (true) {
             String line = scanner.nextLine().trim();
             if (line.isEmpty()) break;
